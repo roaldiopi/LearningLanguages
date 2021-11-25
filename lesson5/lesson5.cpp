75dea5323859ba3e40714694d3c17b7b5ee3b4ee
roaldiopi <75036913+roaldiopi@users.noreply.github.com>
2021-11-25 11:59:38 +0200

    Add files via upload

---
+++ lesson5/lesson5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int age = 20;
+
+int main() {
+    
+    // cout << ::age; Вывод глабальной переменной
+    //:: - оператор области видимости
+    //double farengeit, celsii;
+
+    // cout << "Введите температуру в градусах Фаренгейта: ";
+    // cin >> farengeit;
+
+    // celsii = (farengeit - 32) / 1.8;
+    // cout << setprecision(2) << setiosflags(ios::fixed);
+    // cout << "Температура в градусах Цельсия: " << celsii << endl;
+    //--------------------------------------------------------------
+    // F = 1.8 * C + 32
+    // cout << "Введите температуру в градусах Цельсия: ";
+    // cin >> celsii;
+
+    // farengeit = 1.8 * celsii + 32;
+
+    // cout << setprecision(2) << setiosflags(ios::fixed);
+
+    // cout << "Температура в градусах Фаренгейта: " << farengeit << endl;
+    //--------------------------------------------------------------
+    // double mdl, eur, usd;
+
+    // cout << "Введите количество леев Молдавии: ";
+    // cin >> mdl;
+    // eur = mdl / 19.98;
+    // usd =  mdl / 17.76;
+    // cout << setprecision(2) << setiosflags(ios::fixed);
+    // cout << "Количество евро: " << eur << endl;
+    // cout << "Количество долларов: " << usd << endl;
+    //--------------------------------------------------------------
+    cout << "END";
+    
+
+
+    
+
+    return 0;
+}
